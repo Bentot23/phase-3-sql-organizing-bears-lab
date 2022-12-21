62c7aebaf187aa8a7fe6612550c386cf712de28a
Bentot23 <92287648+Bentot23@users.noreply.github.com>
2022-12-20 23:07:17 -0600

    commit tree ea90474b6c8580362a6d793ce507c37809cc2029

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Mr. Chocolate', 15, 'M', 'Brown', 'Gentle', 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Rowdy', 13, 'M', 'Black', 'Curios', 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Tabitha', 14, 'F', 'Brown', 'Affectionate', 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Sergeant Brown', 17, 'M', 'Brown', 'Protective', 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Melissa', 10, 'F', 'White', 'Joyful', 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Grinch', 13, 'M', 'Black', 'Aggresive', 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Wendy', 16, 'M', 'White', 'Social', 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES(NULL, 20, 'F', 'Black', 'Fearful', 0);
